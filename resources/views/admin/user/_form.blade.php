83707ee37f473b798a2ec404f88e3d774baf070f
rhodesiax <rhodesiax.lo@gmail.com>
2019-04-28 12:00:06 +0800

    aaa

--- resources/views/admin/user/_form.blade.php
+++ resources/views/admin/user/_form.blade.php
@@ -1,27 +1,27 @@
 <div class="form-group">
     <label for="tag" class="col-md-3 control-label">用户名</label>
     <div class="col-md-5">
-        <input type="text" class="form-control" name="name" id="tag" value="{{ $name }}" autofocus>
+        <input type="text" class="form-control" name="name" id="tag1" value="{{ $name }}" autofocus>
     </div>
 </div>
 <div class="form-group">
     <label for="tag" class="col-md-3 control-label">邮箱</label>
     <div class="col-md-5">
-        <input type="text" class="form-control" name="email" id="tag" value="{{ $email }}" autofocus>
+        <input type="text" class="form-control" name="email" id="tag2" value="{{ $email }}" autofocus>
     </div>
 </div>
 
 <div class="form-group">
     <label for="tag" class="col-md-3 control-label">密码</label>
     <div class="col-md-5">
-        <input type="password" class="form-control" name="password" id="tag" value="" autofocus>
+        <input type="password" class="form-control" name="password" id="tag3" value="" autofocus>
     </div>
 </div>
 
 <div class="form-group">
     <label for="tag" class="col-md-3 control-label">密码确认</label>
     <div class="col-md-5">
-        <input type="password" class="form-control" name="password_confirmation" id="tag" value="" autofocus>
+        <input type="password" class="form-control" name="password_confirmation" id="tag4" value="" autofocus>
     </div>
 </div>
 
