fdff7ad84a64640d1f2ff7fa4603c8287608067a
nanming <3077302585@qq.com>
2019-04-29 15:02:02 +0800

    add excel 4/29 am 03

--- resources/views/pos/excel/index.blade.php
+++ resources/views/pos/excel/index.blade.php
@@ -7,30 +7,63 @@
 @section('pageDesc','DashBoard')
 
 @section('content')
-    <p> 导入数据页面1 </p>
-    <form id="f" enctype="multipart/form-data">
-        <input type="file" name="file"><br/>
-        <input type="button" id="btn" value="提交">
+    <!-- <form id="f" enctype="multipart/form-data" class='txal' style='margin:200px 0;'>
+        <div class='rel'>
+            <input type="file" name="file" class='abs' style='width:300px;border:1px solid #c5bdbd;display:inline-block;opacity:0;'>
+            <input type="" name="" class='bor-4' placeholder='选择上传文件' style='width:300px;border:1px solid #baf9de;display:inline-block;z-index:-1;'>
+        </div>
+        <input type="button" id="btn" class='bor-n bg_green_1 white bor-4' value="上传" style='margin:10px 0;width:100px;'>
+    </form> -->
+
+    <form enctype="multipart/form-data" id="uploadImg">
+        上传文件:  
+        <input name="file" type="file" id="file"> 
     </form>
+    <div onclick='upload1()'>upload</div>
 @stop
 
 @section('js')
     <script>
-        $(function () {
-            $("#btn").on("click",function () {
-                //使用FormData对象来提交整个表单，它支持文件的上传
-                var formData=new FormData(document.getElementById("f"));
-                //使用ajax提交
-                $.ajax("/pos/excel/index",{
-                    type:"POST",
-                    data:formData,
-                    processData:false,//告诉jquery不要去处理请求的数据格式
-                    contentType:false,//告诉jquery不要设置请求头的类型
-                    success:function (data) {
-                        console.log(data);
-                    }
-                });
-            })
+        // $(function () {
+        //     $("#btn").on("click",function () {
+        //         var formData=new FormData(document.getElementById("f"));
+        //         debugger
+        //         $.ajax("/pos/excel/index",{
+        //             type:"POST",
+        //             data:formData,
+        //             processData:false,
+        //             contentType:false,
+        //             success:function (data) {
+        //                 console.log(data);
+        //             }
+        //         });
+        //     })
+        // })
+        let file1;
+        $(function(){
+            $('input[type="file"]').on('change', function(){
+                var file = this.files[0];
+                var formData = new FormData($('#uploadImg')[0]);
+                formData.append('file', file);
+                console.log(formData.get('file'))
+                file1=formData
+            });
         })
+        function upload1(){
+            $.ajax({
+                url: '/pos/excel/index',
+                type: 'POST',
+                cache: false,
+                data: file1,
+                //dataType: 'json',
+                //async: false,
+                processData: false,
+                contentType: false,
+            }).done(function(res) {
+                console.log(res)
+            }).fail(function(res) {
+                console.log(res)
+            });
+        }
     </script>
 @stop
